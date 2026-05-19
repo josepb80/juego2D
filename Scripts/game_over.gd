5e1ca10e5f69587635095b0266c069a3f79dfdf3
josepb80 <jbarrera@elpuig.xeill.net>
2026-05-19 10:32:42 +0200

    Web y game over

--- Scripts/game_over.gd
+++ Scripts/game_over.gd
@@ -12,3 +12,18 @@ func _on_retry_pressed():
 func _on_menu_pressed():
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+
+func _ready():
+	var sprite = $AnimatedSprite2D
+	sprite.play("default")
+	
+	var frames_totales = sprite.sprite_frames.get_frame_count("default")
+	var fps = sprite.sprite_frames.get_animation_speed("default")
+	var duracion = frames_totales / fps
+	
+	# Creamos un temporizador que espere exactamente ese tiempo
+	await get_tree().create_timer(duracion).timeout
+	
+	# Frenazo de mano: detenemos y clavamos el último frame
+	sprite.stop()
+	sprite.frame = frames_totales - 1
